1ba3ee1ed26f43ebd6aadfddb22766f430a05f3b
ShounaKnk <shounakkannaik0607@gmail.com>
2026-03-29 19:23:46 +0530

    imporved the RaceSchedules dict to support sprint races

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import fastf1
 import pandas as pd
+import json, os
 
 def get_calendar(year: int):
-    schedule = fastf1.get_event_schedule(2026)
-    calendar = schedule[["EventName", "Session1", "Session1DateUtc", "Session2", "Session2DateUtc", "Session3", "Session3DateUtc", "Session4", "Session4DateUtc", "Session5", "Session5DateUtc"]]
-    PreSeasonTesting_calendar = calendar[:2]
-    race_calendar = calendar[2:]
+    schedule = fastf1.get_event_schedule(2025)
+    calendar = schedule[["EventName", "EventFormat", "Session1", "Session1DateUtc", "Session2", "Session2DateUtc", "Session3", "Session3DateUtc", "Session4", "Session4DateUtc", "Session5", "Session5DateUtc"]]
+    race_calendar = calendar[calendar["EventFormat"].isin(["conventional", "sprint_qualifying"])]
     racetimes = ["Session1DateUtc", "Session2DateUtc", "Session3DateUtc", "Session4DateUtc", "Session5DateUtc"]
     race_calendar[racetimes] = race_calendar[racetimes].apply(
         lambda col: col.dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata')
@@ -17,11 +17,11 @@ def get_race_schedules(year):
     race_calendar = get_calendar(year)
     for index, race in race_calendar.iterrows():
         RaceSchedules[race["EventName"]] = {
-                    "FP1": race["Session1DateUtc"],
-                    "FP2": race["Session2DateUtc"],
-                    "FP3": race["Session3DateUtc"],
-                    "Qualifying": race["Session4DateUtc"],
-                    "Race": race["Session5DateUtc"]
+                    race["Session1"]: race["Session1DateUtc"],
+                    race["Session2"]: race["Session2DateUtc"],
+                    race["Session3"]: race["Session3DateUtc"],
+                    race["Session4"]: race["Session4DateUtc"],
+                    race["Session5"]: race["Session5DateUtc"]
                 }
     # for key, value in RaceSchedules.items():
     #     print(f'{key}: {value["Race"]}')
@@ -49,18 +49,23 @@ def create_event(race, raceTime, isMainEvent):
             },
         'reminders': reminders
     }
-    for key, value in event.items():
-        print(f"{key}: {value}")
-            
+    if os.path.exists("events.json"):
+        with open("events.json","a") as f:
+            json.dump(event, f)
+            f.write("\n")
+    return event
     # code to add google calendar event
 
 def add_race_schedule_to_calendar(year: int):
     race_schedule = get_race_schedules(year)
+    calendarEventList = []
     for EventName, races in race_schedule.items():
-        create_event(EventName, races["FP1"], isMainEvent=True)
+        grandPrix = create_event(EventName, races["Practice 1"], isMainEvent=True)
+        calendarEventList.append(grandPrix)
         for race, time in races.items():
-            create_event(race=race, raceTime=time, isMainEvent=False)
-
+            RaceEvent = create_event(race=race, raceTime=time, isMainEvent=False)
+            calendarEventList.append(RaceEvent)
+    print(len(calendarEventList))
 # print(WeekendSchedule.keys())
 
 add_race_schedule_to_calendar(2026)
