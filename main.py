e4121e273c405bcf6aa6821e112923b375c531cd
ShounaKnk <shounakkannaik0607@gmail.com>
2026-03-29 13:02:58 +0530

    converted the timestamps to IST, adjusted the reminder times based on the race types

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def get_calendar(year: int):
     calendar = schedule[["EventName", "Session1", "Session1DateUtc", "Session2", "Session2DateUtc", "Session3", "Session3DateUtc", "Session4", "Session4DateUtc", "Session5", "Session5DateUtc"]]
     PreSeasonTesting_calendar = calendar[:2]
     race_calendar = calendar[2:]
+    racetimes = ["Session1DateUtc", "Session2DateUtc", "Session3DateUtc", "Session4DateUtc", "Session5DateUtc"]
+    race_calendar[racetimes] = race_calendar[racetimes].apply(
+        lambda col: col.dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata')
+    )
     return race_calendar
 
 def get_race_schedules(year):
@@ -19,27 +23,44 @@ def get_race_schedules(year):
                     "Qualifying": race["Session4DateUtc"],
                     "Race": race["Session5DateUtc"]
                 }
-    for key, value in RaceSchedules.items():
-        print(f'{key}: {value["Race"]}')
-        return RaceSchedules
+    # for key, value in RaceSchedules.items():
+    #     print(f'{key}: {value["Race"]}')
+    
+    return RaceSchedules
 
-def create_event(race, timestamp, isMainEvent):
+def create_event(race, raceTime, isMainEvent):
+    minutes_list = [1440]if isMainEvent else [120, 90, 15]
+    start= (raceTime - pd.Timedelta(hours=2)).isoformat() if isMainEvent else raceTime.isoformat()
+    reminders = {
+        'userDefault': False,
+        'overrides': [
+            {'method': 'popup', 'minutes': m} for m in minutes_list
+        ]
+    }
     event={
         'summary': race,
-        'start': {'datetime': timestamp},
-        'end': {'datetime': timestamp + pd.Timedelta(hours=5)},
+        'start': {
+                'datetime': start,
+                'timeZone': 'Asia/Kolkata'
+            },
+        'end': {
+                'datetime': (raceTime + pd.Timedelta(hours=2)).isoformat(),
+                'timeZone': 'Asia/Kolkata'
+            },
+        'reminders': reminders
     }
+    for key, value in event.items():
+        print(f"{key}: {value}")
+            
     # code to add google calendar event
-    
-    if isMainEvent:
-        'reminders'= {
-            'userDefault': False,
-            'overrides': [
-                {'method': 'popup', 'minutes': 1440},
-                {'method': 'popup', 'minutes': 120}
-            ]
-        }
+
+def add_race_schedule_to_calendar(year: int):
+    race_schedule = get_race_schedules(year)
+    for EventName, races in race_schedule.items():
+        create_event(EventName, races["FP1"], isMainEvent=True)
+        for race, time in races.items():
+            create_event(race=race, raceTime=time, isMainEvent=False)
 
 # print(WeekendSchedule.keys())
-    
 
+add_race_schedule_to_calendar(2026)
